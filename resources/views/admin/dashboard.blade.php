8c0efd3a6ed91465bc05e7735cbe3a1d9dd8d1c6
Phong Tranh <phongtranh68@gmail.com>
2016-01-26 00:39:31 +0700

    create dashboard template

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,24 @@
 @extends('admin/admin_app')
 
 @section('content')
-Dashboard
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h5>DASHBOARD</h5></div>
+
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div><!-- /sidebar -->
+        <div class="col-md-9">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div><!-- /content -->
+    </div>
+</div>
 @endsection()
