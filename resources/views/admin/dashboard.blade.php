6b7ef42d9f584ee311fc8bfdd55a053b194f9c60
Phong Tranh <phongtranh68@gmail.com>
2016-01-26 01:03:38 +0700

    update dashboard template

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,11 +4,14 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-3">
-            <div class="panel panel-default">
+            <div class="panel panel-default" id="main-sidebar">
                 <div class="panel-heading"><h5>DASHBOARD</h5></div>
-
                 <div class="panel-body">
-                    You are logged in!
+                    <ul class="list-group menu-list">
+                        <li><a href="#" class="list-group-item">Users</a></li>
+                        <li><a class="list-group-item" href="#">Articles</a></li>
+                        <li><a class="list-group-item" href="#">Setting</a></li>
+                    </ul>
                 </div>
             </div>
         </div><!-- /sidebar -->
